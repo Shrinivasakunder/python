d54cc7eff5308d98696e9375b02a138a232a2626
Shrinivasa kunder <53564657+Shrinivasakunder@users.noreply.github.com>
2020-03-15 11:19:09 +0530

    Update Micro and Array Update .py

--- Micro and Array Update .py
+++ Micro and Array Update .py
@@ -1,12 +1,13 @@
-a=[]
-t=int(input("Enter the no. of test cases:"))
+t = int(input("Enter the no. of test cases:"))
 for i in range(t):
-    n=int(input("Enter the no. of elements:"))
-    k=int(input("Enter the k value:"))
+    a=[]
+    n = int(input("Enter the no. of elements:"))
+    k = int(input("Enter the k value:"))
     for j in range(n):
-      a[j]=int(input())
+        a.append(int(input("Enter Element into list:")))
     a.sort()
-    if k<=a[0]:
+    print(a)
+    if k <= a[0]:
         print("0")
     else:
-        print(k-a[0])
+        print(k - a[0])
